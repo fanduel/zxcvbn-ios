a660cae0c23939f2b0c0cefe416e1608b17b370b
Steven King <steven.king@fanduel.com>
2017-05-17 17:13:42 +0100

    Calculate `guesses` for `DBDateMatch`

--- Zxcvbn/DBDateMatch.m
+++ Zxcvbn/DBDateMatch.m
@@ -16,18 +16,17 @@ static int kNumYears = 119; // years match against 1900 - 2019
 static int kNumMonths = 12;
 static int kNumDays = 31;
 
-- (CGFloat)entropy
-{
-    float entropy = 0.0;
+- (NSUInteger)guesses {
+    NSUInteger result = 1;
     if (self.year < 100) {
-        entropy = lg(kNumDays * kNumMonths * 100); // two-digit year
+        result = kNumDays * kNumMonths * 100; // two-digit year
     } else {
-        entropy = lg(kNumDays * kNumMonths * kNumYears); // four-digit year
+        result = kNumDays * kNumMonths * kNumYears; // four-digit year
     }
     if ([self.separator length]) {
-        entropy += 2; // add two bits for separator selection [/,-,.,etc]
+        result *= 4; // add two bits for separator selection [/,-,.,etc]
     }
-    return entropy;
+    return result;
 }
 
 @end

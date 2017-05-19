311dfe4ef3fc1589976c43dfcc63290b82c977db
Steven King <steven.king@fanduel.com>
2017-05-19 15:00:36 +0100

    Add brute force match subclass of `DBMatch`

---
+++ Zxcvbn/DBBruteForceMatch.h
@@ -0,0 +1,15 @@
+//
+//  DBBruteForceMatch.h
+//  Zxcvbn
+//
+//  Created by Steven King on 19/05/2017.
+//  Copyright © 2017 Dropbox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "DBMatch.h"
+
+@interface DBBruteForceMatch : DBMatch
+
+@end
---
+++ Zxcvbn/DBBruteForceMatch.m
@@ -0,0 +1,13 @@
+//
+//  DBBruteForceMatch.m
+//  Zxcvbn
+//
+//  Created by Steven King on 19/05/2017.
+//  Copyright © 2017 Dropbox. All rights reserved.
+//
+
+#import "DBBruteForceMatch.h"
+
+@implementation DBBruteForceMatch
+
+@end

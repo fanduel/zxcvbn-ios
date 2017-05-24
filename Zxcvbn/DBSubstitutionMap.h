f7f784a6dfe9f5f940a57ea5479f9a030bca82f8
Steven King <steven.king@fanduel.com>
2017-05-24 11:20:51 +0100

    Add `DBSubstitutionMap`

---
+++ Zxcvbn/DBSubstitutionMap.h
@@ -0,0 +1,13 @@
+//
+//  DBL33tDictionary.h
+//  Zxcvbn
+//
+//  Created by Steven King on 23/05/2017.
+//  Copyright © 2017 Dropbox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBSubstitutionMap : NSObject
+
+@end
---
+++ Zxcvbn/DBSubstitutionMap.m
@@ -0,0 +1,13 @@
+//
+//  DBL33tDictionary.m
+//  Zxcvbn
+//
+//  Created by Steven King on 23/05/2017.
+//  Copyright © 2017 Dropbox. All rights reserved.
+//
+
+#import "DBSubstitutionMap.h"
+
+@implementation DBSubstitutionMap
+
+@end

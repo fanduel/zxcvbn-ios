c93949535d2119ae65ff165a6b03fdd2a92e4a3b
Steven King <steven.king@fanduel.com>
2017-05-29 11:07:13 +0100

    Add `DBSequenceMatcher`

---
+++ Zxcvbn/DBSequenceMatcher.m
@@ -0,0 +1,22 @@
+//
+//  DBSequenceMatcher.m
+//  Zxcvbn
+//
+//  Created by Steven King on 16/05/2017.
+//  Copyright © 2017 Dropbox. All rights reserved.
+//
+
+#import "DBSequenceMatcher.h"
+
+@interface DBSequenceMatcher ()
+
+
+@end
+
+@implementation DBSequenceMatcher
+
+- (NSArray<DBMatch *> *)matchesForPassword:(NSString *)password {
+    return @[];
+}
+
+@end
---
+++ ZxcvbnTests/DBSequenceMatcherTests.m
@@ -0,0 +1,19 @@
+//
+//  DBSequenceMatcherTests.m
+//  Zxcvbn
+//
+//  Created by Steven King on 29/05/2017.
+//  Copyright © 2017 Dropbox. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "DBSequenceMatcher.h"
+
+@interface DBSequenceMatcherTests : XCTestCase
+
+@end
+
+@implementation DBSequenceMatcherTests
+
+@end

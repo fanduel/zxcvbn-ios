4875e30f623eb15e38b16ebdb2d2acf3b95b2a2a
Steven King <steven.king@fanduel.com>
2017-05-30 17:10:01 +0100

    Add `DBPermutationMatcher`

---
+++ Zxcvbn/DBPermutationMatcher.h
@@ -0,0 +1,15 @@
+//
+//  DBPermutationMatcher.h
+//  Zxcvbn
+//
+//  Created by Steven King on 30/05/2017.
+//  Copyright © 2017 Dropbox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "DBMatching.h"
+
+@interface DBPermutationMatcher : NSObject <DBMatching>
+
+@end
---
+++ ZxcvbnTests/DBPermutationMatcherTests.m
@@ -0,0 +1,17 @@
+//
+//  DBPermutationMatcherTests.m
+//  Zxcvbn
+//
+//  Created by Steven King on 30/05/2017.
+//  Copyright © 2017 Dropbox. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DBPermutationMatcherTests : XCTestCase
+
+@end
+
+@implementation DBPermutationMatcherTests
+
+@end

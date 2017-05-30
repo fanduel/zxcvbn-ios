df026860286df3c6860cfdcab84bccb1ebd242f9
Steven King <steven.king@fanduel.com>
2017-05-30 15:42:18 +0100

    Add `DBMatcherCache`

---
+++ Zxcvbn/DBMatcherCache.m
@@ -0,0 +1,21 @@
+//
+//  DBMatcherCache.m
+//  Zxcvbn
+//
+//  Created by Steven King on 17/05/2017.
+//  Copyright © 2017 Dropbox. All rights reserved.
+//
+
+#import "DBMatcherCache.h"
+
+@interface DBMatcherCache ()
+
+@end
+
+@implementation DBMatcherCache
+
+- (NSArray<DBMatch *> *)matchesForPassword:(NSString *)password {
+    return @[];
+}
+
+@end
---
+++ ZxcvbnTests/DBMatcherCacheTests.m
@@ -0,0 +1,19 @@
+//
+//  DBMatcherCacheTests.m
+//  Zxcvbn
+//
+//  Created by Steven King on 30/05/2017.
+//  Copyright © 2017 Dropbox. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "DBMatcherCache.h"
+
+@interface DBMatcherCacheTests : XCTestCase
+
+@end
+
+@implementation DBMatcherCacheTests
+
+@end
